f62556a5fd42438a74d6c7e29b4cbf0bf51cfb4f
IngVincent <lazvinz21@gmail.com>
2022-04-01 12:27:43 -0600

    controller hotfix in Delete function

--- com.gestion.empleados/src/main/java/com/gestion/empleados/controllador/EmpleadoControlador.java
+++ com.gestion.empleados/src/main/java/com/gestion/empleados/controllador/EmpleadoControlador.java
@@ -1,11 +1,14 @@
 package com.gestion.empleados.controllador;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 import com.gestion.empleados.modelo.Empleado;
 import com.gestion.empleados.repositorio.EmpleadoRepositorio;
@@ -61,7 +66,17 @@ public class EmpleadoControlador {
 		Empleado empleadoActualizado = repositorio.save(empleado);
 		return ResponseEntity.ok(empleadoActualizado);
 	}
-
+	//este metodo sirve para eliminar un empleado
+	@DeleteMapping("/empleados/{id}")
+	public ResponseEntity<Map<String,Boolean>> eliminarEmpleado(@PathVariable Long id){
+		Empleado empleado = repositorio.findById(id)
+				            .orElseThrow(() -> new ResourcesNotFoundException("No existe el empleado con el ID : " + id));
+		
+		repositorio.delete(empleado);
+		Map<String, Boolean> respuesta = new HashMap<>();
+		respuesta.put("eliminar",Boolean.TRUE);
+		return ResponseEntity.ok(respuesta);
+    }	
 	
 	
 	
